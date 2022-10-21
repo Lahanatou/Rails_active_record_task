c0c097caead030cc75fdd4e92794b42d7cdf6a03
lahanatou <nouryous30@gmail.com>
2022-10-21 01:07:20 +0100

    first commit

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,27 +1,40 @@
 class ExercisesController < ApplicationController
-  def exercise1
+   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  def index
     # 【要件】注文されていないすべての料理を返すこと
     #   * left_outer_joinsを使うこと
-    @foods = Food
+    foods = Food.left_outer_joins(:orders)
+foods.each do |food|
+  puts food.name
+end
   end
 
   def exercise2
     # 【要件】注文されていない料理を提供しているすべてのお店を返すこと
-    #   * left_outer_joinsを使うこと
-    @shops = Shop
+    #   * left_outer_joinsを使うことS
+    shops = Shop.left_outer_joins(:addresses)
+shops.each do |shop|
+  puts shop.name
+end
   end
 
-  def exercise3 
+  def exercise3
     # 【要件】配達先の一番多い住所を返すこと
     #   * joinsを使うこと
     #   * 取得したAddressのインスタンスにorders_countと呼びかけると注文の数を返すこと
-    @address = Address
+    address = Address.joins(:orders)
+   address.each do |shop|
+     puts address.name
+
+     # puts address.orders_count
+   end
+
   end
 
-  def exercise4 
+  def exercise4
     # 【要件】一番お金を使っている顧客を返すこと
     #   * joinsを使うこと
     #   * 取得したCustomerのインスタンスにfoods_price_sumと呼びかけると合計金額を返すこと
-    @customer = Customer
+    # @customer = Customer
   end
 end
